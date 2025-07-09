db9bcd5580ca10591a729a6e5167c28ccfaa4b68
Massimo Manghi <mxmanghi@apache.org>
2025-07-09 15:54:51 +0200

    change text with greeting message to comply with test script

--- tests/test_webapp_home.py
+++ tests/test_webapp_home.py
@@ -4,5 +4,5 @@ from starlette.testclient import TestClient
 def test_home_contains_message(test_client: TestClient):
     response = test_client.get("/")
     assert response.status_code == 200
-    target_message = "Hello, world!"
+    target_message = "Hi there!"
     assert target_message in response.text
