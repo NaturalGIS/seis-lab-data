7e05422f5163e095e4ef0533fc2df787b27d5773
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2025-07-29 23:57:03 +0100

    fixed failing unit test

--- tests/test_webapp_home.py
+++ tests/test_webapp_home.py
@@ -2,7 +2,7 @@ from starlette.testclient import TestClient
 
 
 def test_home_contains_message(test_client: TestClient):
-    response = test_client.get("/")
+    response = test_client.get("/", params={"lang": "en"})
     assert response.status_code == 200
     target_message = "Hi there!"
     assert target_message in response.text
