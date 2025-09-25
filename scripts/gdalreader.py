929bd3498fc83ded8a3effc700c039ca1d1c73d1
Massimo Manghi <mxmanghi@apache.org>
2025-09-25 11:46:10 +0200

    removing __str__ and demoting extract_metadata to simple function

--- scripts/gdalreader.py
+++ scripts/gdalreader.py
@@ -1,13 +1,8 @@
-# -*- coding: utf-8 -*-
-""" """
-
 import argparse
 import dataclasses
 from dataclasses import field
-from dataclasses import fields
 from datetime import datetime
 from datetime import UTC
-import json
 import os
 from osgeo import gdal
 from osgeo import ogr
@@ -28,173 +23,170 @@ class Metadata:
     error_type: str = ""
     messages: list[str] = field(default_factory=list)
 
-    def __str__(self):
-        md_d = dict()
-        for f in fields(Metadata):
-            md_d[f.name] = getattr(self, f.name)
 
-        return json.dumps(md_d)
+warning_notes = []
 
 
-def gdal_open_exceptions_handler(cls, err_class, err_no, msg):
+def gdal_open_exceptions_handler(err_class, err_no, msg):
+    global warning_notes
     if err_class == gdal.CE_Warning:
-        GDALReader.warning_notes.append(msg)
+        warning_notes.append(msg)
 
 
-class GDALReader:
+def extract_metadata(path):
+    global warning_notes
+
+    gdal.UseExceptions()
+
     warning_notes = []
+    metadata = Metadata(
+        os.path.basename(path),
+        os.path.getsize(path),
+        datetime.fromtimestamp(os.path.getctime(path), UTC).isoformat() + "Z",
+    )
 
-    def extract_metadata(self, path):
-        gdal.UseExceptions()
+    # Try opening as raster
 
-        metadata = Metadata(
-            os.path.basename(path),
-            os.path.getsize(path),
-            datetime.fromtimestamp(os.path.getctime(path), UTC).isoformat() + "Z",
-        )
+    gdal.PushErrorHandler(gdal_open_exceptions_handler)
+    try:
+        ds = gdal.Open(path, gdal.GA_ReadOnly)
+    except Exception:
+        metadata.error_type = gdal.GetLastErrorType()
+        metadata.error_no = gdal.GetLastErrorNo()
+        metadata.messages.append(gdal.GetLastErrorMsg())
+        return metadata
 
-        # Try opening as raster
+    gdal.PopErrorHandler()
 
-        gdal.PushErrorHandler(gdal_open_exceptions_handler)
+    if ds is not None:
         try:
-            ds = gdal.Open(path, gdal.GA_ReadOnly)
-        except Exception:
-            metadata.error_type = gdal.GetLastErrorType()
-            metadata.error_no = gdal.GetLastErrorNo()
-            metadata.messages.append(gdal.GetLastErrorMsg())
-            return metadata
+            drv = ds.GetDriver()
+            metadata.format = drv.LongName
+            metadata.driver = drv.ShortName
 
-        gdal.PopErrorHandler()
+            # Spatial reference
 
-        if ds is not None:
-            try:
-                drv = ds.GetDriver()
-                metadata.format = drv.LongName
-                metadata.driver = drv.ShortName
+            # get data projection if any
 
-                # Spatial reference
+            proj = ds.GetProjection()
+            srs = osr.SpatialReference(wkt=proj) if proj else None
+            epsg = None
+            if srs and srs.IsProjected():
+                srs.AutoIdentifyEPSG()
+                try:
+                    epsg = int(srs.GetAttrValue("AUTHORITY", 1))
+                except Exception:
+                    pass
 
-                # get data projection if any
+            # get the GeoTransform and infer the extent from it
 
-                proj = ds.GetProjection()
-                srs = osr.SpatialReference(wkt=proj) if proj else None
-                epsg = None
-                if srs and srs.IsProjected():
-                    srs.AutoIdentifyEPSG()
-                    try:
-                        epsg = int(srs.GetAttrValue("AUTHORITY", 1))
-                    except Exception:
-                        pass
+            gt = ds.GetGeoTransform()
+            if gt:
+                xmin = gt[0]
+                ymax = gt[3]
+                xmax = xmin + ds.RasterXSize * gt[1]
+                ymin = ymax + ds.RasterYSize * gt[5]
+            else:
+                xmin = ymin = xmax = ymax = None
 
-                # get the GeoTransform and infer the extent from it
+            metadata.spatial = {
+                "crs": {"epsg": epsg, "wkt": proj},
+                "extent": {"xmin": xmin, "ymin": ymin, "xmax": xmax, "ymax": ymax},
+                "resolution": {
+                    "x": gt[1] if gt else None,
+                    "y": gt[5] if gt else None,
+                },
+                "geometry_type": None,
+            }
 
-                gt = ds.GetGeoTransform()
-                if gt:
-                    xmin = gt[0]
-                    ymax = gt[3]
-                    xmax = xmin + ds.RasterXSize * gt[1]
-                    ymin = ymax + ds.RasterYSize * gt[5]
-                else:
-                    xmin = ymin = xmax = ymax = None
-
-                metadata.spatial = {
-                    "crs": {"epsg": epsg, "wkt": proj},
-                    "extent": {"xmin": xmin, "ymin": ymin, "xmax": xmax, "ymax": ymax},
-                    "resolution": {
-                        "x": gt[1] if gt else None,
-                        "y": gt[5] if gt else None,
-                    },
-                    "geometry_type": None,
-                }
-
-                # Raster bands
-                bands_info = []
-                for i in range(1, ds.RasterCount + 1):
-                    band = ds.GetRasterBand(i)
-                    stats = band.GetStatistics(True, True)
-                    bands_info.append(
-                        {
-                            "band": i,
-                            "datatype": gdal.GetDataTypeName(band.DataType),
-                            "nodata_value": band.GetNoDataValue(),
-                            "statistics": {
-                                "min": stats[0],
-                                "max": stats[1],
-                                "mean": stats[2],
-                                "stddev": stats[3],
-                            },
-                        }
-                    )
-
-                metadata.raster = {"bands": ds.RasterCount, "info": bands_info}
-
-                ds = None
-            except Exception:
-                print(f"not handling {path} as raster")
-
-            metadata.messages = GDALReader.warning_notes
-            return metadata
-
-        # Try opening as vector
-        try:
-            ds_vec = ogr.Open(path, 0)
-            if ds_vec is not None:
-                raise Exception("Unable to open the file as vector data file")
-
-            print(f"reading {path} as vector file with driver")
-            try:
-                driver = ds_vec.GetDriver().GetName()
-            except Exception:
-                driver = None
-                print(f"failed to establish a driver for {path}")
-
-            metadata.driver = driver
-            metadata.format = driver
-
-            layers = []
-            for i in range(ds_vec.GetLayerCount()):
-                layer = ds_vec.GetLayer(i)
-                srs = layer.GetSpatialRef()
-                epsg = None
-                if srs:
-                    try:
-                        srs.AutoIdentifyEPSG()
-                        epsg = int(srs.GetAttrValue("AUTHORITY", 1))
-                    except Exception:
-                        pass
-
-                extent = layer.GetExtent()
-                schema = {}
-                defn = layer.GetLayerDefn()
-                for j in range(defn.GetFieldCount()):
-                    fld = defn.GetFieldDefn(j)
-                    schema[fld.GetName()] = fld.GetFieldTypeName(fld.GetType())
-
-                layers.append(
+            # Raster bands
+            bands_info = []
+            for i in range(1, ds.RasterCount + 1):
+                band = ds.GetRasterBand(i)
+                stats = band.GetStatistics(True, True)
+                bands_info.append(
                     {
-                        "name": layer.GetName(),
-                        "geometry_type": ogr.GeometryTypeToName(layer.GetGeomType()),
-                        "feature_count": layer.GetFeatureCount(),
-                        "attributes": schema,
-                        "extent": {
-                            "xmin": extent[0],
-                            "ymin": extent[2],
-                            "xmax": extent[1],
-                            "ymax": extent[3],
-                        },
-                        "crs": {
-                            "epsg": epsg,
-                            "wkt": srs.ExportToWkt() if srs else None,
+                        "band": i,
+                        "datatype": gdal.GetDataTypeName(band.DataType),
+                        "nodata_value": band.GetNoDataValue(),
+                        "statistics": {
+                            "min": stats[0],
+                            "max": stats[1],
+                            "mean": stats[2],
+                            "stddev": stats[3],
                         },
                     }
                 )
 
-            metadata.vector = {"layers": layers}
-            ds_vec = None
-        except Exception as e:
-            print(f"Error reading vector data from {path}: {e}")
+            metadata.raster = {"bands": ds.RasterCount, "info": bands_info}
 
+            ds = None
+        except Exception:
+            print(f"not handling {path} as raster")
+
+        metadata.messages = warning_notes
         return metadata
+
+    # Try opening as vector
+    try:
+        ds_vec = ogr.Open(path, 0)
+        if ds_vec is not None:
+            raise Exception("Unable to open the file as vector data file")
+
+        print(f"reading {path} as vector file with driver")
+        try:
+            driver = ds_vec.GetDriver().GetName()
+        except Exception:
+            driver = None
+            print(f"failed to establish a driver for {path}")
+
+        metadata.driver = driver
+        metadata.format = driver
+
+        layers = []
+        for i in range(ds_vec.GetLayerCount()):
+            layer = ds_vec.GetLayer(i)
+            srs = layer.GetSpatialRef()
+            epsg = None
+            if srs:
+                try:
+                    srs.AutoIdentifyEPSG()
+                    epsg = int(srs.GetAttrValue("AUTHORITY", 1))
+                except Exception:
+                    pass
+
+            extent = layer.GetExtent()
+            schema = {}
+            defn = layer.GetLayerDefn()
+            for j in range(defn.GetFieldCount()):
+                fld = defn.GetFieldDefn(j)
+                schema[fld.GetName()] = fld.GetFieldTypeName(fld.GetType())
+
+            layers.append(
+                {
+                    "name": layer.GetName(),
+                    "geometry_type": ogr.GeometryTypeToName(layer.GetGeomType()),
+                    "feature_count": layer.GetFeatureCount(),
+                    "attributes": schema,
+                    "extent": {
+                        "xmin": extent[0],
+                        "ymin": extent[2],
+                        "xmax": extent[1],
+                        "ymax": extent[3],
+                    },
+                    "crs": {
+                        "epsg": epsg,
+                        "wkt": srs.ExportToWkt() if srs else None,
+                    },
+                }
+            )
+
+        metadata.vector = {"layers": layers}
+        ds_vec = None
+    except Exception as e:
+        print(f"Error reading vector data from {path}: {e}")
+
+    return metadata
 
 
 # Example usage
@@ -205,8 +197,7 @@ if __name__ == "__main__":
     parser.add_argument("file_name", help="GDAL supported file path")
     args = parser.parse_args()
 
-    gdal_reader = GDALReader()
-    m = gdal_reader.extract_metadata(args.file_name)
+    m = extract_metadata(args.file_name)
     # print(json.dumps(m, indent=2))
     print(str(m))
     print(f"#### {args.file_name} ####")
