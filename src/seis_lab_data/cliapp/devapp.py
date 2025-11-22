45e2c69e5c8439a5b79b7f69290ec1a9ca4bd2ca
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2025-11-22 13:24:17 +0000

    Generate large volumes of data for testing (#69)
    Added devapp command to generate large number of synthetic data

--- src/seis_lab_data/cliapp/devapp.py
+++ src/seis_lab_data/cliapp/devapp.py
@@ -1,3 +1,5 @@
+from typing import Annotated
+
 import typer
 from sqlalchemy.exc import IntegrityError
 
@@ -7,6 +9,7 @@ from .. import (
     schemas,
 )
 from ..db import queries
+from ..events.emitters import null_emitter
 from . import sampledata
 from .asynctyper import AsyncTyper
 
@@ -16,6 +19,83 @@ app = AsyncTyper()
 @app.callback()
 def dev_app_callback(ctx: typer.Context):
     """Dev-related commands"""
+
+
+@app.async_command()
+async def generate_many_projects(
+    ctx: typer.Context,
+    num_projects: Annotated[
+        int,
+        typer.Option(
+            help=(
+                "Number of projects to generate. Beware - the bigger this number, the longer this command "
+                "will take to run"
+            ),
+            min=1,
+            max=100,
+        ),
+    ] = 10,
+    enable_event_emitter: bool = False,
+):
+    """Generate synthetic data"""
+    session_maker = ctx.obj["session_maker"]
+    created = []
+    settings = ctx.obj["main"].settings
+    emitter = (
+        events.get_event_emitter(settings) if enable_event_emitter else null_emitter
+    )
+    async with session_maker() as session:
+        dataset_categories = await queries.collect_all_dataset_categories(session)
+        workflow_stages = await queries.collect_all_workflow_stages(session)
+        domain_types = await queries.collect_all_domain_types(session)
+        project_generator = sampledata.generate_sample_projects(
+            owners=[schemas.UserId("fake_user")],
+            dataset_categories=[
+                schemas.DatasetCategoryId(dc.id) for dc in dataset_categories
+            ],
+            domain_types=[schemas.DomainTypeId(dt.id) for dt in domain_types],
+            workflow_stages=[schemas.WorkflowStageId(ws.id) for ws in workflow_stages],
+        )
+        for i in range(num_projects):
+            ctx.obj["main"].status_console.print(
+                f"Creating project {i + 1}/{num_projects}..."
+            )
+            project_to_create, missions_info = next(project_generator)
+            created.append(
+                await operations.create_project(
+                    project_to_create,
+                    initiator=ctx.obj["admin_user"],
+                    session=session,
+                    settings=settings,
+                    event_emitter=emitter,
+                )
+            )
+            for mission_index, (mission_to_create, records_to_create) in enumerate(
+                missions_info
+            ):
+                ctx.obj["main"].status_console.print(
+                    f"\tCreating mission ({mission_index + 1}/{len(missions_info)}) for project..."
+                )
+                await operations.create_survey_mission(
+                    mission_to_create,
+                    initiator=ctx.obj["admin_user"],
+                    session=session,
+                    settings=settings,
+                    event_emitter=emitter,
+                )
+                for record_index, record_to_create in enumerate(records_to_create):
+                    ctx.obj["main"].status_console.print(
+                        f"\t\tCreating record ({record_index + 1}/{len(records_to_create)}) for mission..."
+                    )
+                    await operations.create_survey_related_record(
+                        record_to_create,
+                        initiator=ctx.obj["admin_user"],
+                        session=session,
+                        settings=settings,
+                        event_emitter=emitter,
+                    )
+            ctx.obj["main"].status_console.print("--------")
+        ctx.obj["main"].status_console.print("Done!")
 
 
 @app.async_command()
@@ -49,8 +129,8 @@ async def load_sample_projects(ctx: typer.Context):
                     f"Project {to_create.id!r} already exists, skipping..."
                 )
                 await session.rollback()
-    for created_campaign in created:
-        to_show = schemas.ProjectReadListItem(**created_campaign.model_dump())
+    for created_project in created:
+        to_show = schemas.ProjectReadListItem(**created_project.model_dump())
         ctx.obj["main"].status_console.print(to_show)
 
 
