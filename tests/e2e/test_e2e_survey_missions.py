0578776ffeb61a7f9adad54f032d7e8282034e16
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2025-12-23 12:51:21 +0000

    Simplify tests

--- tests/e2e/test_e2e_survey_missions.py
+++ tests/e2e/test_e2e_survey_missions.py
@@ -20,50 +20,6 @@ def test_survey_mission_lifecycle(authenticated_page: Page):
     authenticated_page.get_by_role("textbox", name="field-name-en").fill(
         "e2e test project"
     )
-    authenticated_page.get_by_role("textbox", name="field-name-pt").fill(
-        "projeto de teste e2e"
-    )
-    authenticated_page.get_by_role("textbox", name="field-description-en").fill(
-        "e2e test description"
-    )
-    authenticated_page.get_by_role("textbox", name="field-description-pt").fill(
-        "descrição do projeto de teste"
-    )
-    authenticated_page.get_by_role("textbox", name="field-root_path").fill("/somewhere")
-
-    authenticated_page.get_by_role("button", name="add-another-link").click()
-    authenticated_page.get_by_role("textbox", name="field-link-links-0-url").fill(
-        "http://fakelink.com"
-    )
-    authenticated_page.get_by_role(
-        "textbox", name="field-link-links-0-media_type"
-    ).fill("text/html")
-    authenticated_page.get_by_role("textbox", name="field-link-links-0-relation").fill(
-        "fake-relation"
-    )
-    authenticated_page.get_by_role(
-        "textbox", name="field-link-links-0-link_description-en"
-    ).fill("some link description")
-    authenticated_page.get_by_role(
-        "textbox", name="field-link-links-0-link_description-pt"
-    ).fill("uma descrição do link")
-
-    authenticated_page.get_by_role("button", name="add-another-link").click()
-    authenticated_page.get_by_role("textbox", name="field-link-links-1-url").fill(
-        "http://fakelink2.com"
-    )
-    authenticated_page.get_by_role(
-        "textbox", name="field-link-links-1-media_type"
-    ).fill("text/html")
-    authenticated_page.get_by_role("textbox", name="field-link-links-1-relation").fill(
-        "fake-relation2"
-    )
-    authenticated_page.get_by_role(
-        "textbox", name="field-link-links-1-link_description-en"
-    ).fill("some description for link 2")
-    authenticated_page.get_by_role(
-        "textbox", name="field-link-links-1-link_description-pt"
-    ).fill("uma descrição do segundo link")
 
     authenticated_page.get_by_role("button", name="submit-create-form").click()
 
