00f89105e97f4b1db7732f847ee1cb03358a5c3a
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2026-02-04 13:51:36 +0000

    Start adding admin guide

---
+++ docs/admin-guide.pt.md
@@ -0,0 +1,78 @@
+# Administração do sistema
+
+<span class="no-pdf">
+    [ :material-file-pdf-box: Descarregar versão PDF](assets/documents/seis-lab-data-administration-guide.pdf){ .md-button .no-pdf }
+</span>
+
+Este documento contem um breve guia para auxílio na manutenção do sistema SeisLabData.
+
+
+## Ambiente de produção
+
+O sistema SeisLabData está disponível dentro da rede interna do IPMA. O seu ponto de
+entrada para os utilizadores é através do URL:
+
+<https://seis-lab-data.ipma.pt>
+
+
+### Componentes do sistema
+
+O sistema SeisLabData é composto pelos seguintes componentes:
+
+```mermaid
+graph TD
+    rev-proxy(1. reverse proxy)
+    webapp(2. web application)
+    db[(3. main system db)]
+    proc-worker(4. processing worker)
+    file-server(5. http file server)
+    tile-server(6. map tiles server)
+    auth-webapp(7. user authentication service)
+    auth-db[(7.1. database for authentication service)]
+    auth-worker(7.2. worker for authentication service)
+    message-broker(10. message broker)
+    arch[[11. archive mount]]
+    rev-proxy --> webapp
+    rev-proxy --> file-server
+    rev-proxy --> tile-server
+    rev-proxy --- auth-webapp
+    auth-webapp <--> auth-db
+    auth-webapp --> auth-worker
+    auth-worker <--> auth-db
+    webapp <--> db
+    proc-worker <--> db
+    webapp <--> message-broker
+    message-broker <--> proc-worker
+    arch --> file-server
+    arch --> tile-server
+    arch <--> proc-worker
+```
+
+#### 1. `reverse proxy` service
+
+This service routes incoming HTTP requests to the appropriate handler. It performs the following
+routing:
+
+| Rule | Destination |
+| ---- | ----------- |
+| requests to `seis-lab-data.ipma.pt` | Routed to the `web application` service |
+| requests to `auth.seis-lab-data.ipma.pt` | Routed to the `user authentication service` |
+| requests to `arch.seis-lab-data.ipma.pt` | Routed to the `http file server` |
+| requests to `seis-lab-data.ipma.pt/tiles` | Routed to the `map tiles server` |
+
+
+##### Relevant configuration files
+
+
+#### 2. `web application` service
+#### 3. `main system db`
+
+
+## Operational System Deployment
+
+
+## Management operations
+
+### seis-lab-data CLI tool
+
+### start/stop the system
--- mkdocs_hooks.py
+++ mkdocs_hooks.py
@@ -1,5 +1,6 @@
 """Custom mkdocs hooks for generating PDF documents"""
 
+import dataclasses
 from pathlib import Path
 import os
 import shlex
@@ -9,48 +10,70 @@ import subprocess
 from mkdocs.structure.files import File
 
 
-_destination_relative_path = "assets/documents"
-_pdf_already_generated = False
-_pdf_name = "seis-lab-data-development-guide.pdf"
+@dataclasses.dataclass(frozen=True)
+class PdfResourceConfig:
+    pdf_name: str
+    mkdocs_conf: str
+    build_dir_name: str
+
+
+PDF_RESOURCES = (
+    PdfResourceConfig(
+        pdf_name="seis-lab-data-development-guide.pdf",
+        mkdocs_conf="mkdocs-pdf-dev-guide.yml",
+        build_dir_name="site-dev-guide-pdf",
+    ),
+    PdfResourceConfig(
+        pdf_name="seis-lab-data-administration-guide.pdf",
+        mkdocs_conf="mkdocs-pdf-admin-guide.yml",
+        build_dir_name="site-admin-guide-pdf",
+    ),
+)
+
+
+_DESTINATION_RELATIVE_PATH = "assets/documents"
+_pdfs_already_generated = False
 
 
 def on_config(config):
-    if not os.getenv("BUILD_PDF"):
+    if not os.getenv("BUILD_PDFS"):
         return config
 
-    global _pdf_already_generated
-    global _pdf_name
-    global _destination_relative_path
+    global _pdfs_already_generated
+    global _DESTINATION_RELATIVE_PATH
 
-    if _pdf_already_generated:
-        print("PDF already generated")
+    if _pdfs_already_generated:
+        print("PDFs already generated")
         return config
 
-    print("Generating PDF...")
-    build_dir = Path(__file__).parent / "site-temp-pdf"
-    try:
-        subprocess.run(
-            shlex.split(
-                f"mkdocs build "
-                f"--config-file mkdocs-pdf-dev-guide.yml "
-                f"--site-dir {build_dir}"
-            ),
-            check=True,
-        )
-    except subprocess.CalledProcessError as err:
-        print(f"Error generating PDF: {str(err)}")
-    else:
-        src_pdf_path = build_dir / f"pdf/{_pdf_name}"
-        dest_dir = Path(__file__).parent / f"docs/{_destination_relative_path}"
-        dest_pdf_path = dest_dir / _pdf_name
-
-        dest_dir.mkdir(parents=True, exist_ok=True)
-        if src_pdf_path.is_file():
-            shutil.copy(src_pdf_path, dest_pdf_path)
-            print(f"PDF successfully moved to {dest_pdf_path}")
-            shutil.rmtree(build_dir, ignore_errors=True)
+    print("Generating PDFs...")
+    for pdf_resource_conf in PDF_RESOURCES:
+        build_dir = Path(__file__).parent / pdf_resource_conf.build_dir_name
+        try:
+            subprocess.run(
+                shlex.split(
+                    f"mkdocs build "
+                    f"--config-file {pdf_resource_conf.mkdocs_conf} "
+                    f"--site-dir {build_dir}"
+                ),
+                check=True,
+            )
+        except subprocess.CalledProcessError as err:
+            print(f"Error generating PDF {pdf_resource_conf.pdf_name}: {str(err)}")
         else:
-            print(f"Could not move PDF to {dest_pdf_path}")
+            src_pdf_path = build_dir / f"pdf/{pdf_resource_conf.pdf_name}"
+            dest_dir = Path(__file__).parent / f"docs/{_DESTINATION_RELATIVE_PATH}"
+            dest_pdf_path = dest_dir / pdf_resource_conf.pdf_name
+
+            dest_dir.mkdir(parents=True, exist_ok=True)
+            if src_pdf_path.is_file():
+                shutil.copy(src_pdf_path, dest_pdf_path)
+                print(f"PDF successfully moved to {dest_pdf_path}")
+                shutil.rmtree(build_dir, ignore_errors=True)
+            else:
+                print(f"Could not move PDF to {dest_pdf_path}")
+    else:
+        _pdfs_already_generated = True
     return config
 
 
@@ -59,18 +82,18 @@ def on_files(files, config):
     Explicitly add the generated PDF to the MkDocs file list
     so the i18n plugin and link validator can see it.
     """
-    global _destination_relative_path
-    global _pdf_name
-    path = "/".join((_destination_relative_path, _pdf_name))
+    global _DESTINATION_RELATIVE_PATH
 
-    # Only add it if it's not already in the files list
-    if not any(f.src_path == path for f in files):
-        new_file = File(
-            path=path,
-            src_dir=config["docs_dir"],
-            dest_dir=config["site_dir"],
-            use_directory_urls=config["use_directory_urls"],
-        )
-        files.append(new_file)
+    for pdf_resource in PDF_RESOURCES:
+        path = "/".join((_DESTINATION_RELATIVE_PATH, pdf_resource.pdf_name))
+        # Only add it if it's not already in the files list
+        if not any(f.src_path == path for f in files):
+            new_file = File(
+                path=path,
+                src_dir=config["docs_dir"],
+                dest_dir=config["site_dir"],
+                use_directory_urls=config["use_directory_urls"],
+            )
+            files.append(new_file)
 
     return files
